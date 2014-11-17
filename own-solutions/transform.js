a097ce85612e6a36498d27f13517e6a919a2a74f
Tom@Pegasus <info@thomas-mundt.de>
2014-11-17 19:24:34 +0100

    excercise#4 cleaned up (logs), completed

--- own-solutions/transform.js
+++ own-solutions/transform.js
@@ -2,22 +2,14 @@ var through = require('through');
 var throughStream = through(write, end);
 process.stdin.pipe(throughStream).pipe(process.stdout);
 
-//throughStream.end();
-
+// Write buffer to stream
 function write(buffer) {
-<<<<<<< HEAD
 	var stringData = buffer.toString().toUpperCase();
-	//console.log("function write(), stringData ist: " + stringData);
 	this.queue(stringData);
-=======
-	this.queue(buffer).toString().toUpperCase();
-	console.log('queue ist: '+this.queue);
->>>>>>> 3eb306f9a4e5fac266c7c57ebfff8c85108fe543
 }
 
+// end stream/throughStream
 function end() {
 	this.queue(null);
 	throughStream.end();
 }
-
-//throughStream.end();
